89f7b1e1a51a86a733745dc1ef647a7112c5feba
payallmoney <payallmoney@163.com>
2014-09-06 14:42:46 +0800

    all

--- 提交博客.cmd
+++ 提交博客.cmd
@@ -1,6 +1,4 @@
 @cd /D D:\workspace\payallmoney.github.io
 @git add -A
 @git commit -a -m all
-"C:\Program Files\TortoiseGit\bin\TortoiseGitProc.exe" /command:push /path:"D:\workspace\payallmoney.github.io\"
-
-@pause
+@"C:\Program Files\TortoiseGit\bin\TortoiseGitProc.exe" /command:push /path:"D:\workspace\payallmoney.github.io\"
